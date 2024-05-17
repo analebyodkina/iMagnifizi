252fb2e9e8c715bb0695ee6e21781fdcb7a7387e
analebyodkina <dp030992cad@gmail.com>
2024-05-17 12:02:04 +0300

    making second sidebar

--- js/script.js
+++ js/script.js
@@ -10,3 +10,8 @@ function sideRight() {
   var sideRight = document.querySelector('.sidebar._right');
   sideRight.classList.toggle("active");
 }
+// left
+function sideLeft() {  
+  var sideLeft = document.querySelector('.sidebar._left');
+  sideLeft.classList.toggle("active");
+}
