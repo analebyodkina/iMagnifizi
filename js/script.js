54527a3c6794dda6f7f8990a8d8a552a7c4abc47
analebyodkina <dp030992cad@gmail.com>
2024-05-17 11:47:21 +0300

    making sidebar navigation

--- js/script.js
+++ js/script.js
@@ -3,3 +3,10 @@ function navToggle() {
     var headerUl = document.getElementById("headerUl");
     headerUl.classList.toggle("active");
   }
+
+// right sidebar
+function sideRight() {
+  
+  var sideRight = document.querySelector('.sidebar._right');
+  sideRight.classList.toggle("active");
+}
