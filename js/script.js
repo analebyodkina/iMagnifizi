4e34217ef6a2c8ac759e40fb8e91c0da5d97d606
analebyodkina <dp030992cad@gmail.com>
2024-05-17 12:53:18 +0300

    fixed left sidebar  openeing

--- js/script.js
+++ js/script.js
@@ -6,12 +6,15 @@ function navToggle() {
 
 // right sidebar
 function sideRight() {
-  
+  var sideLeft = document.querySelector('.sidebar._left');
   var sideRight = document.querySelector('.sidebar._right');
   sideRight.classList.toggle("active");
+  sideLeft.classList.remove("active");
 }
 // left
 function sideLeft() {  
   var sideLeft = document.querySelector('.sidebar._left');
+  var sideRight = document.querySelector('.sidebar._right');
   sideLeft.classList.toggle("active");
+  sideRight.classList.remove("active");
 }
